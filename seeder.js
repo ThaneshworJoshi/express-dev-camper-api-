3ba91f8d39af4e6e163f7394c6e6afb89824106e
Thaneshwor <thaneshwor100@gmail.com>
2021-04-06 15:33:48 +0545

    Courses CRUD,seeder,routes and bootcamp pagination

--- seeder.js
+++ seeder.js
@@ -8,6 +8,9 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
+const course = require('./models/Course');
+
+const Course = require('./models/Course');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,15 +24,20 @@ mongoose.connect(process.env.MONGO_URI, {
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    // await Course.create(courses);
+
     console.log('Data Imported'.green.inverse);
     process.exit();
   } catch (error) {
-    console.error(err);
+    console.error(error);
   }
 };
 
@@ -37,10 +45,12 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
+
     console.log('Data Destroyed'.red.inverse);
     process.exit();
   } catch (error) {
-    console.error(err);
+    console.error(error);
   }
 };
 
