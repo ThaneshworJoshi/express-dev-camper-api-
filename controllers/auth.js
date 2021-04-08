a5500c228ebc262693bdfcee65f7cb8c4de5a1e8
Thaneshwor <thaneshwor100@gmail.com>
2021-04-08 17:53:05 +0545

    remove unwanted code

--- controllers/auth.js
+++ controllers/auth.js
@@ -102,11 +102,6 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
     return next(new ErrorResponse('Email could not be sent', 500));
   }
-
-  res.status(200).json({
-    success: true,
-    date: user,
-  });
 });
 
 // Get token from model, create cookie and send response
