ed14c9629d0db90066f3d55b349c8953df959d33
Thaneshwor <thaneshwor100@gmail.com>
2021-04-09 18:35:50 +0545

    Add xss-clean package to prevent xss attack

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
@@ -33,6 +34,9 @@ app.use(mongoSanitize());
 
 // Set security headers
 app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Cookie Parser
 app.use(cookieParser());
